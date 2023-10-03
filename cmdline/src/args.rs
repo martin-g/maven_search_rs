5fece0359a22ae3e1e1bd779d0430686bf87e45b
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2023-10-03 11:44:03 +0300

    cargo fmt
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- cmdline/src/args.rs
+++ cmdline/src/args.rs
@@ -4,8 +4,9 @@ use getargs::{Arg, Options};
 
 use maven_search_lib::types::{MavenResult, MavenSearchArgs};
 
-pub fn get_args<'a, I : Iterator<Item = &'a str>>(opts: &'a mut Options<&'a str, I>) -> MavenResult<'a, MavenSearchArgs<'a>>
-{
+pub fn get_args<'a, I: Iterator<Item = &'a str>>(
+    opts: &'a mut Options<&'a str, I>,
+) -> MavenResult<'a, MavenSearchArgs<'a>> {
     let mut res = MavenSearchArgs::default();
 
     while let Some(opt) = opts.next_arg().expect("argument parsing error") {
@@ -13,7 +14,9 @@ pub fn get_args<'a, I : Iterator<Item = &'a str>>(opts: &'a mut Options<&'a str,
             Arg::Long("version") => res.show_version = true,
             Arg::Short('u') | Arg::Long("check-for-update") => res.check_for_update = true,
             Arg::Short('h') | Arg::Long("help") => res.show_help = true,
-            Arg::Short('f') | Arg::Long("format") => res.format = opts.value().expect("format argument parsing error"),
+            Arg::Short('f') | Arg::Long("format") => {
+                res.format = opts.value().expect("format argument parsing error")
+            }
             Arg::Positional(search_term) => res.search_term = Some(search_term),
             arg => panic!("Unknown option: {arg}"),
         }
