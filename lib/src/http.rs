c63ecd7d11173e9c7b692be27fc27288262d2851
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-07-21 22:52:50 +0300

    urlencode the query parameter
    Add env_logger for the tests
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- lib/src/http.rs
+++ lib/src/http.rs
@@ -1,13 +1,14 @@
 use crate::types::{Doc, HttpResponse, MavenResult};
 
 pub fn search<'a>(query: &str) -> MavenResult<'a, Vec<Doc>> {
-    let url = format!("https://search.maven.org/solrsearch/select?start=0&rows=1&q={query}");
+    let query = urlencoding::encode(query);
+    let url = &format!("https://search.maven.org/solrsearch/select?start=0&rows=1&q={query}");
 
-    debug!("Going to make a request to : {:?}", &url);
+    debug!("Going to make a request to : {url:?}");
 
-    let resp: HttpResponse = ureq::get(url.as_str()).call()?.into_json()?;
+    let resp: HttpResponse = ureq::get(url).call()?.body_mut().read_json()?;
 
-    debug!("response:\n{:?}", &resp);
+    debug!("response:\n{resp:?}");
     Ok(resp.response.docs)
 }
 
@@ -17,6 +18,8 @@ mod tests {
 
     #[test]
     fn test_search() {
+        env_logger::init();
+
         let docs = search("g:org.apache.wicket AND a:wicket-core").unwrap();
         assert_eq!(docs.len(), 1);
         let doc = &docs[0];
